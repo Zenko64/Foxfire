0d110e3cb9ca388cd4706904175441ec88ab6f45
Simão Correia <simi.correia@outlook.com>
2026-08-22 02:10:55 +0100

    create onboarding step

--- apps/frontend/src/components/Layout.tsx
+++ apps/frontend/src/components/Layout.tsx
@@ -1,12 +1,25 @@
 import { Outlet } from "react-router";
+import { authClient } from "@/lib/auth";
 import { Navbar } from "./Navbar";
+import { SetupScreen } from "./profile/Setup";
 import { Toaster } from "./ui/toast";
 
 export function Layout() {
+	const { data } = authClient.useSession();
+
 	return (
 		<>
 			<Navbar />
 			<main>
+				{data?.user &&
+					(!data?.user.displayUsername || !data?.user.username) && (
+						<SetupScreen
+							data={{
+								displayUsername: data?.user.displayUsername ?? "",
+								username: data?.user.username ?? "",
+							}}
+						/>
+					)}
 				<Outlet />
 			</main>
 			<Toaster />
