e371923e16e9eddee8f156552e556c4ca1b16104
Simão Correia <simi.correia@outlook.com>
2026-08-22 22:58:00 +0100

    remove useless baseUrl

--- apps/frontend/src/lib/auth.ts
+++ apps/frontend/src/lib/auth.ts
@@ -3,7 +3,6 @@ import { anonymousClient, usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-	baseURL: "http://localhost:5173",
 	basePath: "/api/auth",
 	plugins: [usernameClient(), anonymousClient()],
 });
