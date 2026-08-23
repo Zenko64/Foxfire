079c6b064f66c689c4caf6dac881a92f6afa4a0f
Simão Correia <simi.correia@outlook.com>
2026-08-23 22:50:56 +0100

    add user search route

--- apps/backend/src/auth/http/routes.ts
+++ apps/backend/src/auth/http/routes.ts
@@ -6,7 +6,8 @@
  */
 import { factory } from "../../core/http/factory";
 import { auth } from "../../lib/auth";
-import { getUser } from "../service";
+import { BadRequestError } from "../../lib/errors";
+import { getUser, getUsers } from "../service";
 
 const authRouter = factory
 	.createApp({
@@ -17,6 +18,15 @@ const authRouter = factory
 
 		return c.json(await getUser({ username }));
 	})
+	.get("/users", async (c) => {
+		const searchQuery = c.req.query("search");
+		if (!searchQuery)
+			throw new BadRequestError(
+				"The search query parameter is missing in the request path.",
+			);
+		return c.json(await getUsers(searchQuery, 5));
+	})
+
 	.on(["POST", "GET"], "/*", (c) => auth.handler(c.req.raw));
 
 export default authRouter;
